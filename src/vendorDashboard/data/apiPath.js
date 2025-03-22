8d8940c633e42931c1fd616de8ce7b1c23def11f
srikanth-gandra <srikanthgandra6@gmail.com>
2025-03-22 12:59:36 +0530

    activating live url

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:7000";
+// export const API_URL = "http://localhost:7000";
 
-// export const API_URL = "https://backend-nodejs-suby-uscw.onrender.com"
+export const API_URL = "https://backend-nodejs-suby-uscw.onrender.com"
